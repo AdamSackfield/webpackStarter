c3acaccd22a053369d9920f17c97517ed4600d06
AdamSackfield <=>
2018-07-03 18:42:01 +0100

    Added Nav component

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 
 import classes from './App.scss'
+import Navigation from '../components/Navigation/Navgation';
 
   class App extends Component {
     render() {
       return (
         <div className={classes.Main}>
-          App.js
+          <Navigation />
         </div>
       )
     }
